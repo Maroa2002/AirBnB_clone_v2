da972ea64552dc840685730d52303741a705b457
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-19 12:16:26 +0300

    typo error removal

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- granting all privileges to user
-GRANT ALL PRIVILEGES ON hbnb_test_dn.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- granting select privilege on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
