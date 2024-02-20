df600e91d895b6409c008953936334d14bb08578
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-20 10:52:12 +0300

    changes to the dev and test database setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,12 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- granting user all privileges
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- granting select privilege
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- flush privileges
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,7 +7,12 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- granting all privileges to user
+USE hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- granting select privilege on performance_schema
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- flush privileges
+FLUSH PRIVILEGES;
