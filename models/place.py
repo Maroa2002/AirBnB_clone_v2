3040d83488bfc1b4535cfaa4bdcab34784f89fec
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-20 13:48:42 +0300

    error removal

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 
 class Place(BaseModel, Base):
     """ A place to stay """
