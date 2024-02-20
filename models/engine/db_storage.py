e88e1f45a900034f78b786a67e17fdd62841de1c
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-20 11:54:43 +0300

    added close method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -60,3 +60,7 @@ class DBStorage:
         sess = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess)
         self.__session = Session()
+
+    def close(self):
+        """method to to call remove method"""
+        self.__session.close()
